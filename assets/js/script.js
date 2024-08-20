8c0ef48db355e51984da6d18474207e8d9681331
Gregory Champkin <wlhpz57z@students.codeinstitute.net>
2024-08-20 07:57:07 +0000

    create battle.html and update h1 and p content

---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+function showLearn() {
+    document.getElementById("learn").innerHTML = "As you know, 'Ser' and 'Estar' in Spanish both mean 'to be' in English. So when do you use each one? Remembering the anagrams DOCTOR(ser) and PLACE(estar) will help you to get it right each time. Revise them below then head to battle to test your knowledge. ¡Venga!"
+};
