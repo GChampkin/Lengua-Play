d61da9c157e7e349d18955d2aaf803afe703a793
Gregory Champkin <wlhpz57z@students.codeinstitute.net>
2024-08-30 06:37:32 +0000

    add function for displaying random phrase

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,72 +1,91 @@
 const generatePhrase = document.getElementById("battle-button"); // create element to apply an event listener to
 const outputElement = document.getElementById("random-sentence"); // create element for generating random phrase
+const strikeButton = document.getElementById("strike-button"); // create element to apply event listener to
+let input = document.querySelector("#answer");
+let serPoints = document.getElementById("correct");
+let estarPoints = document.getElementById("incorrect");
 
-const randomPhrases = [{
-    serPhrases: [
-        "Yo soy/estoy alto.", 
-        "Tú eres/estás gracioso.", 
-        "El perro es/está grande.", 
-        "Nosotros somos/estamos una familia.",
-        "Los museos son/están interesantes.",
-        "Yo soy/estoy abogado.",
-        "Tu eres/estás profesor.",
-        "Mi hermana es/está dentista.",
-        "Mis padres son/están doctores.",
-        "Yo soy/estoy una persona muy amable."
-        "Mi madre es/está tán simpática.",
-        "Vosotros sois/estáis guapos.",
-        "Tu eres/estás muy delgado.",
-        "Mis abuelos son/están generosos.",
-        "¿Qué hora son? - es/está la una de la tarde.",
-        "¿Qué hora son? - son/están las diez de la noche."
-        "¿Qué hora son? - son/están las ocho de la mañana.",
-        "Yo soy/estoy de Inglaterra.",
-        "Tú eres/estás de Venezuela.",
-        "Esta falda es/está de México.",
-        "Nostros somos/estamos de Francia.",
-        "Mis padres son/están de los Estados Unidos.",
-        "Yo soy/estoy tu amigo.",
-        "Él es/está mi hermano.",
-        "Nosotros somos/estamos una familia",
-        "Ellos son/están mis padres."
-    ]
-},
-{
-    estarPhrases: [
-        "La farmácia es/está cerca.",
-        "El banco es/está en frente del gimnasio.",
-        "Soy/estoy detrás de la mesa.",
-        "Mis gafas son/están debajo de la silla.",
-        "Yo soy/estoy en el sur de Inglaterra.",
-        "Mi profe es/está en el colegio.",
-        "Mi madre es/está en el pueblo.",
-        "¿Sois/estáis en la fiesta?",
-        "Mis amigos son/están de vacaciones.",
-        "Yo soy/estoy hablando con mi amiga.",
-        "¿Eres/estás comiendo con nosotros?",
-        "Mi padre es/está trabajando ahora.",
-        "Mis amigos y yo somos/estamos dando un paseo.",
-        "Mis padres son/están aprendiendo español.",
-        "Soy/estoy muy cansado.",
-        "Es/está un poco enfermo.",
-        "Somos/estamos llenos de energia.",
-        "Mis perros son/están durmiendo.",
-        "Hoy, soy/estoy muy contenta.",
-        "Mi hermano es/está triste.",
-        "Por qué sois/estáis enfedados?",
-        "Los niños son/están emocionantes.",  
-    ]
-}
-];
+const randomPhrases = [
+        {phrase: "Yo soy/estoy alto.", correctAnswer: "soy", incorrectAnswer: "estoy"}, 
+        {phrase: "Tú eres/estás gracioso.",  correctAnswer: "eres", incorrectAnswer: "estás"},
+        {phrase: "El perro es/está grande.", correctAnwer: "es", incorrectAnswer: "está"},
+        {phrase: "Nosotros somos/estamos una familia.", correctAnswer: "somos", incorrectAnswer: "estamos"},
+        {phrase: "Los museos son/están interesantes.", correctAnswer: "son", incorrectAnswer: "están"},
+        {phrase: "Yo soy/estoy abogado.", correctAnswer: "soy", incorrectAnswer: "estoy"},
+        {phrase: "Tu eres/estás profesor.", correctAnswer: "eres", incorrectAnswer: "estás"},
+        {phrase: "Mi hermana es/está dentista.", correctAnswer: "es", incorrectAnswer: "está"},
+        {phrase: "Mis padres son/están doctores.", correctAnswer: "son", incorrectAnswer: "están"},
+        {phrase: "Yo soy/estoy una persona muy amable.", correctAnswer: "soy", incorrectAnswer: "estoy"},
+        {phrase: "Mi madre es/está tán simpática.", correctAnswer: "es", incorrectAnswer: "está"},
+        {phrase: "Vosotros sois/estáis guapos.", correctAnswer: "sois", incorrectAnswer: "estáis"},
+        {phrase: "Tu eres/estás muy delgado.", correctAnswer: "eres", incorrectAnswer: "estás"},
+        {phrase: "Mis abuelos son/están generosos.", correctAnswer: "son", incorrectAnswer: "están"},
+        {phrase: "¿Qué hora son? - es/está la una de la tarde.", correctAnswer: "es", incorrectAnswer: "está"},
+        {phrase: "¿Qué hora son? - son/están las diez de la noche.", correctAnswer: "son", incorrectAnswer: "están"},
+        {phrase: "¿Qué hora son? - son/están las ocho de la mañana.", correctAnswer: "son", incorrectAnswer: "están"},
+        {phrase: "Yo soy/estoy de Inglaterra.", correctAnswer: "soy", incorrectAnswer: "estoy"},
+        {phrase: "Tú eres/estás de Venezuela.", correctAnswer: "eres", incorrectAnswer: "estás"},
+        {phrase: "Esta falda es/está de México.", correctAnswer: "es", incorrectAnswer: "está"},
+        {phrase: "Nostros somos/estamos de Francia.", correctAnswer: "somos", incorrectAnswer: "estamos"},
+        {phrase: "Mis padres son/están de los Estados Unidos.", correctAnswer: "son", incorrectAnswer: "están"},
+        {phrase: "Yo soy/estoy tu amigo.", correctAnswer: "soy", incorrectAnswer: "estoy"},
+        {phrase: "Él es/está mi hermano.", correctAnswer: "es", incorrectAnswer: "está"},
+        {phrase: "Nosotros somos/estamos una familia",correctAnswer: "somos", incorrectAnswer: "estamos"},
+        {phrase: "Ellos son/están mis padres.", correctAnswer: "son", incorrectAnswer: "están"},
+        {phrase: "La farmácia es/está cerca.", correctAnswer: "está", incorrectAnswer: "es"},
+        {phrase: "El banco es/está en frente del gimnasio.", correctAnswer: "está", incorrectAnswer: "es"},
+        {phrase: "Soy/estoy detrás de la mesa.", correctAnswer: "estoy", incorrectAnswer: "soy"},
+        {phrase: "Mis gafas son/están debajo de la silla.", correctAnswer: "están", incorrectAnswer: "son"},
+        {phrase: "Yo soy/estoy en el sur de Inglaterra.", correctAnswer: "estoy", incorrectAnswer: "soy"},
+        {phrase: "Mi profe es/está en el colegio.", correctAnswer: "está", incorrectAnswer: "es"},
+        {phrase: "Mi madre es/está en el pueblo.", correctAnswer: "está", incorrectAnswer: "es"},
+        {phrase: "¿Sois/estáis en la fiesta?", correctAnswer: "estáis", incorrectAnswer: "sois"},
+        {phrase: "Mis amigos son/están de vacaciones.", correctAnswer: "están", incorrectAnswer: "son"},
+        {phrase: "Yo soy/estoy hablando con mi amiga.", correctAnswer: "estoy", incorrectAnswer: "soy"},
+        {phrase: "¿Eres/estás comiendo con nosotros?", correctAnswer: "estás", incorrectAnswer: "eres"},
+        {phrase: "Mi padre es/está trabajando ahora.", correctAnswer: "está", incorrectAnswer: "es"},
+        {phrase: "Mis amigos y yo somos/estamos dando un paseo.", correctAnswer: "estamos", incorrectAnswer: "somos"},
+        {phrase: "Mis padres son/están aprendiendo español.", correctAnswer: "están", incorrectAnswer: "son"},
+        {phrase: "Soy/estoy muy cansado.", correctAnswer: "estoy", incorrectAnswer: "soy"},
+        {phrase: "Es/está un poco enfermo.", correctAnswer: "está", incorrectAnswer: "es"},
+        {phrase: "Somos/estamos llenos de energia.", correctAnswer: "estamos", incorrectAnswer: "somos"},
+        {phrase: "Mis perros son/están durmiendo.", correctAnswer: "están", incorrectAnswer: "son"},
+        {phrase: "Hoy, soy/estoy muy contenta.", correctAnswer: "estoy", incorrectAnswer: "soy"},
+        {phrase: "Mi hermano es/está triste.", correctAnswer: "está", incorrectAnswer: "es"},
+        {phrase: "Por qué sois/estáis enfedados?", correctAnswer: "estáis", incorrectAnswer: "sois"},
+        {phrase: "Los niños son/están emocionantes.", correctAnswer: "están", incorrectAnswer: "son"}  
+    ];
+let correctValue = null; 
+let incorrectValue = null; 
+// creates event listener on click of "battle" button
 generatePhrase.addEventListener("click", displayRandomPhrase)
-function displayRandomPhrase() {
+
+/**generates random phrase in the html file from phrases array */
+function displayRandomPhrase(event) {
+    event.preventDefault();
     const randomIndex = Math.floor(Math.random() * randomPhrases.length);
-    outputElement.textContent = randomPhrases[randomIndex];
+    outputElement.textContent = randomPhrases[randomIndex].phrase;
+    console.log(randomPhrases[randomIndex]);
 }
 
-const serConjugations = ["soy", "eres", "es", "somos", "sois", "son"];
-const estarConjugations = ["estoy", "estás", "está", "estamos", "estáis", "están"];
+strikeButton.addEventListener("click", checkAnswer)
 
+
+// if user input is equal to correctAnswer; displays correct and generates serPoints
+// if user input is not equal to correctAnswer; displays incorrect and generates estarPoints
+function checkAnswer(event) {
+    event.preventDefault();
+    const randomIndex = Math.floor(Math.random() * randomPhrases.length);
+    correctValue = randomPhrases[randomIndex].correctAnswer;
+    incorrectValue = randomPhrases[randomIndex].incorrectAnswer;
+    if (input.value.toLowerCase() === correctValue) {
+        outputElement.textContent = "Muy bien";
+        console.log("Muy bien")
+    } else if (input.value.toLowerCase() === incorrectValue) {
+        outputElement.textContent = "No es correcto";
+        console.log("no es correcto")
+    }
+}
 
 // function generatePoints: 
 // deciphers if input is correct verb,
@@ -89,13 +108,7 @@ const estarConjugations = ["estoy", "estás", "está", "estamos", "estáis", "es
 //     }
 // })
 
-function runGame() {
 
-}
-
-function checkAnswer() {
-
-}
 
 function findCorrectAnswer() {
 
@@ -106,9 +119,5 @@ function incrementSerPoints() {
 }
 
 function incrementEstarPoints() {
-
-}
-
-function displayRandomPhrase() {
 
 }
